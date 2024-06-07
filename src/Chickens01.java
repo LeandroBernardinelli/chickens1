4a58f2d5951f877a2b658602b1ce198ffd0ce134
ALUNO <ALUNO@LAB04-PC11>
2024-06-07 20:55:51 -0300

    Commit chickens3

---
+++ src/Chickens01.java
@@ -0,0 +1,31 @@
+public class Chickens01 {
+
+    private static int x, y, s, t, q;
+    
+    
+    public static void main(String[] args) {
+        //Put yout code here      
+    int eggsPerChicken = x;
+    int chickenCount = y;
+    
+    int x = 5; 
+    int y = 3;
+    int z = (x*y);
+    
+    int s = z;
+    int t = y++;
+    int q = t / 2;
+    
+    int totalEggs = (s + t + q);
+    
+            
+            
+            
+            
+            
+            
+            
+            
+        System.out.println(totalEggs);
+    }   
+}
